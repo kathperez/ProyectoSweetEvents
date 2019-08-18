0f70247bd91371c1146621d1f4274ae061ccef3b
Luis Arturo Mantilla Quesada <luisart2000@hotmail.com>
2019-08-18 01:45:26 -0600

    JS contactenos

--- ProyectoEventPlanner/app/js/contactenos.js
+++ ProyectoEventPlanner/app/js/contactenos.js
@@ -18,3 +18,8 @@ cerrarVentana.addEventListener('click', function () {
     ventana.classList.remove('active');
 
 });
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
--- ProyectoEventPlanner/app/php/email.php
+++ ProyectoEventPlanner/app/php/email.php
@@ -1,4 +1,5 @@
 <?php
+<?php
 /**
  * @author Cristhian Martinez
  */
@@ -7,16 +8,52 @@ if (isset($_POST['submit'])) {
     $correoDestino = "sweeteventsulacit@gmail.com"; //Definicion del correo al que van a llegar los mensajes
     $asunto = "Validacion de Empresa"; //Asunto de los correos
 
+    function died($error) {
+        echo "Un error se ha generado debido a que se digito erroneamente  <br/>";
+        echo "Por favor corrija el siguiente error: <br/> <br/>";
+        echo $error. "<br/><br/>";
+        die();   
+    }
+    
+    // validar si los datos existen
+
+    if(!isset($_POST['empresa']) ||
+        !isset($_POST['correo']) ||
+        !isset($_POST['telefono'])) {
+        died('Hubo un error a la hora del envio');      
+    }
+
     //Se obtiene el texto de los campos en el form
     $empresa = $_POST['empresa'];
     $correo = $_POST['correo'];
     $telefono = $_POST['telefono'];
+
+    //Exceptions
+    $mensaje_error = "";
+    $correo_excepcion = "/^[0-9]+$/";
+
+    if (!preg_match($correo_excepcion, $correo)) {
+    $mensaje_error .= '<b> El numero de telefono no es valido </b> <br/>';
+    }
+    /*
+    En caso si se necesita pero es la excepcion solo permitir nombre sin numero
+    $empresa_excepcion = "/^[A-Za-z.'-]+$/";   
+    if (!preg_match($empresa_excepcion, $empresa)) {
+        $mensaje_error .= '<b> Nombre de la empresa contiene numeros  </b> <br/>';
+    }
+   */
+    $telefono_excepcion = '/[^\s]*@[a-z0-9.-]*/i';
+
+    if(!preg_match($telefono_excepcion,$telefono)){
+        $mensaje_error .= '<b> La direccion de correo no es valida </b> <br/>';
+    }
 
     //Se le da formato al mensaje
     $mensaje = "Empresa: " . $empresa . "\n" .
     "Correo: " . $correo . "\n" .
     "Telefono: " . $telefono . "\n" .
     "Mensaje: \n";
+    
 
     //Se agrega el texto del mensaje al correo
     $mensaje .= $_POST['mensaje'];
@@ -35,3 +72,4 @@ if (isset($_POST['submit'])) {
     exit;
 
 ?>
+
